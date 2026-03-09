b1c2093bcc4fecf7a44af840a73a60bfdb66f1e1
GET Academy <terje@kolderup.net>
2026-03-09 11:14:09 +0100

    Update CounterEntityFrameworkDbFirstRepository.cs

--- CounterOnionLight.Infrastructure/CounterEntityFrameworkDbFirstRepository.cs
+++ CounterOnionLight.Infrastructure/CounterEntityFrameworkDbFirstRepository.cs
@@ -1,23 +1,66 @@
 ﻿using CounterOnionLight.Core.DomainModel;
 using CounterOnionLight.Core.DomainServices;
+using CounterOnionLight.Infrastructure.Infrastructure.Entities;
+using Counter = CounterOnionLight.Core.DomainModel.Counter;
 
 namespace CounterOnionLight.Infrastructure
 {
-    internal class CounterEntityFrameworkDbFirstRepository : ICounterRepository
+    public class CounterEntityFrameworkDbFirstRepository : ICounterRepository
     {
-        public Task<Counter?> GetAsync(int id)
+        private readonly CounterDbContext _context;
+
+        public CounterEntityFrameworkDbFirstRepository(CounterDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task SaveAsync(Counter counter)
+        public async Task<Counter?> GetAsync(int id)
         {
-            throw new NotImplementedException();
+            var entity = await _context.Counters.FindAsync(id);
+
+            if (entity == null)
+                return null;
+
+            return new Counter
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Value = entity.Value,
+                LastUpdatedBy = entity.LastUpdatedBy,
+                RowVersion = entity.RowVersion
+            };
         }
 
-        public Task AddHistoryAsync(CounterHistoryEntry entry)
+        public async Task SaveAsync(Counter counter)
         {
-            throw new NotImplementedException();
+            var entity = new Counter
+            {
+                Id = counter.Id,
+                Value = counter.Value,
+                LastUpdatedBy = counter.LastUpdatedBy,
+                RowVersion = counter.RowVersion
+            };
+
+            _context.Attach(entity);
+
+            _context.Entry(entity).Property(e => e.Value).IsModified = true;
+            _context.Entry(entity).Property(e => e.LastUpdatedBy).IsModified = true;
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AddHistoryAsync(CounterHistoryEntry entry)
+        {
+            _context.CounterHistories.Add(new CounterHistory
+            {
+                CounterId = entry.CounterId,
+                OldValue = entry.OldValue,
+                NewValue = entry.NewValue,
+                UpdatedBy = entry.UpdatedBy,
+                CreatedUtc = entry.CreatedUtc
+            });
+
+            await _context.SaveChangesAsync();
         }
     }
 }
